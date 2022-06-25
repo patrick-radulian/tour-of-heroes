c53b7305d786e6a4e1ab0129d7285a027d4a9517
Patrick Radulian <patrick.radulian@gmail.com>
2022-06-25 20:35:07 +0200

    Add message service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./components/heroes/heroes.component";
 import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
+import { MessagesComponent } from './components/messages/messages.component';
 
 @NgModule({
-    declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
+    declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
     imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
