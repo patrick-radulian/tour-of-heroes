466e3162a3f64c3dfcaa2f3313e64f4ca2867cf0
Patrick Radulian <patrick.radulian@gmail.com>
2022-06-25 20:35:49 +0200

    Move hero service

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IHero } from "src/app/interfaces/hero";
-import { HeroService } from "src/app/services/hero.service";
+import { HeroService } from "src/app/services/hero/hero.service";
 
 @Component({
     selector: "app-heroes",
--- src/app/services/hero/hero.service.ts
+++ src/app/services/hero/hero.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HEROES } from "src/assets/mock-heroes";
-import { IHero } from "../interfaces/hero";
+import { IHero } from "../../interfaces/hero";
 import { Observable, of } from "rxjs";
 
 @Injectable({
