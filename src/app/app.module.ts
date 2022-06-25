f3ff1bbc3620878c620c1db1221e1c1a07e3628b
Patrick Radulian <patrick.radulian@gmail.com>
2022-06-25 19:17:29 +0200

    Move heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
-import { HeroesComponent } from "./components/heroes/heroes/heroes.component";
+import { HeroesComponent } from "./components/heroes/heroes.component";
+import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
 
 @NgModule({
-    declarations: [AppComponent, HeroesComponent],
+    declarations: [AppComponent, HeroesComponent, HeroDetailComponent],
     imports: [BrowserModule, FormsModule],
     providers: [],
     bootstrap: [AppComponent],
