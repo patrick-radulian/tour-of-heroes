ca843bfd335f4beadcd7c1cc0ec02b811812885e
Patrick Radulian <patrick.radulian@gmail.com>
2022-06-26 13:54:09 +0200

    Setup in-memory-data fake server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,22 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./components/heroes/heroes.component";
 import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
-import { MessagesComponent } from './components/messages/messages.component';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { MessagesComponent } from "./components/messages/messages.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./services/in-memory-data/in-memory-data.service";
 
 @NgModule({
     declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
-    imports: [BrowserModule, FormsModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, { dataEncapsulation: false }),
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
