38d6c9d6d6d58e7e3f851503b9644d7c719f39f1
Patrick Radulian <patrick.radulian@gmail.com>
2022-06-26 14:11:25 +0200

    Make getHero async with http.get and add error handling

--- src/app/services/hero/hero.service.ts
+++ src/app/services/hero/hero.service.ts
@@ -4,6 +4,7 @@ import { IHero } from "../../interfaces/hero";
 import { Observable, of } from "rxjs";
 import { MessageService } from "../message/message.service";
 import { HttpClient } from "@angular/common/http";
+import { catchError, map, tap } from "rxjs/operators";
 
 @Injectable({
     providedIn: "root",
@@ -17,13 +18,40 @@ export class HeroService {
         this.messageService.add(`HeroService: ${message}`);
     }
 
+    /**
+     * Handle Http operation that failed.
+     * Let the app continue.
+     *
+     * @param operation - name of the operation that failed
+     * @param result - optional value to return as the observable result
+     */
+    private handleError<T>(operation = "operation", result?: T) {
+        return (error: any): Observable<T> => {
+            // TODO: send the error to remote logging infrastructure
+            console.error(error); // log to console instead
+
+            // TODO: better job of transforming error for user consumption
+            this.log(`${operation} failed: ${error.message}`);
+
+            // Let the app keep running by returning an empty result.
+            return of(result as T);
+        };
+    }
+
+    /** GET hero by id. Will 404 if id not found */
     getHero(id: number): Observable<IHero> {
-        const hero = HEROES.find((hero) => hero.id === id)!;
-        this.messageService.add(`HeroService: fetched hero id=${id}`);
-        return of(hero);
+        const url = `${this.heroesUrl}/${id}`;
+
+        return this.http.get<IHero>(url).pipe(
+            tap((_) => this.log(`fetched hero id=${id}`)),
+            catchError(this.handleError<IHero>(`getHero id=${id}`))
+        );
     }
 
     getHeroes(): Observable<Array<IHero>> {
-        return this.http.get<Array<IHero>>(this.heroesUrl);
+        return this.http.get<Array<IHero>>(this.heroesUrl).pipe(
+            tap((_) => this.log("fetched heroes")),
+            catchError(this.handleError<Array<IHero>>("getHeroes", []))
+        );
     }
 }
