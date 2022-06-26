a9ab9cea8f5b30ca873a9b94ba6d7d318208a4e2
Patrick Radulian <patrick.radulian@gmail.com>
2022-06-26 12:17:56 +0200

    Update routing to include /dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
 import { HeroesComponent } from "./components/heroes/heroes.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
-const routes: Routes = [{ path: "heroes", component: HeroesComponent }];
+const routes: Routes = [
+    { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+    { path: "heroes", component: HeroesComponent },
+    { path: "dashboard", component: DashboardComponent },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
