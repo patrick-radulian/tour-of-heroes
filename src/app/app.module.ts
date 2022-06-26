a11b4b85a1d1df9a3c921c1fafb53e55103522bf
Patrick Radulian <patrick.radulian@gmail.com>
2022-06-26 12:10:29 +0200

    Add dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { HeroesComponent } from "./components/heroes/heroes.component";
 import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
 import { MessagesComponent } from './components/messages/messages.component';
 import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-    declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
+    declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
     imports: [BrowserModule, FormsModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
