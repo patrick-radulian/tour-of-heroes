8745a02dbd22977813beccf86efb2f9fe901cb68
Patrick Radulian <patrick.radulian@gmail.com>
2022-06-26 13:22:23 +0200

    Add individual hero routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from "@angular/common";
 import { RouterModule, Routes } from "@angular/router";
 import { HeroesComponent } from "./components/heroes/heroes.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
 
 const routes: Routes = [
     { path: "", redirectTo: "/dashboard", pathMatch: "full" },
     { path: "heroes", component: HeroesComponent },
     { path: "dashboard", component: DashboardComponent },
+    { path: "detail/:id", component: HeroDetailComponent },
 ];
 
 @NgModule({
