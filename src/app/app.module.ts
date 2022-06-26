df3cbd4493d9a75bb742d70546b1d669b6f7cf12
Patrick Radulian <patrick.radulian@gmail.com>
2022-06-26 12:02:21 +0200

    Add AppRoutingModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from "./app.component";
 import { HeroesComponent } from "./components/heroes/heroes.component";
 import { HeroDetailComponent } from "./components/hero-detail/hero-detail.component";
 import { MessagesComponent } from './components/messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
-    imports: [BrowserModule, FormsModule],
+    imports: [BrowserModule, FormsModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
